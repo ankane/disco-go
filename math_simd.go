a19d747241382f600996925ebe07d6112bdf6810
Andrew Kane <andrew@ankane.org>
2026-08-24 20:04:01 -0700

    Test

--- math_simd.go
+++ math_simd.go
@@ -7,26 +7,22 @@ import (
 )
 
 func dot(a []float32, b []float32) float32 {
-	dim := len(a)
-	if len(b) < dim {
-		dim = len(b)
-	}
-	i := 0
-	count := dim / 8 * 8
-	var dist archsimd.Float32x8
+	var dist archsimd.Float32x4
 
-	for ; i < count; i += 8 {
-		axs := archsimd.LoadFloat32x8(a[i : i+8])
-		bxs := archsimd.LoadFloat32x8(b[i : i+8])
+	for len(a) >= 4 && len(b) >= 4 {
+		axs := archsimd.LoadFloat32x4(a[:4])
+		bxs := archsimd.LoadFloat32x4(b[:4])
 		dist = axs.MulAdd(bxs, dist)
+		a = a[4:]
+		b = b[4:]
 	}
 
-	var s [8]float32
+	var s [4]float32
 	dist.StoreArray(&s)
-	distance := s[0] + s[1] + s[2] + s[3] + s[4] + s[5] + s[6] + s[7]
+	distance := s[0] + s[1] + s[2] + s[3]
 
-	for i < dim {
-		distance += a[i] * b[i]
+	for i, ai := range a {
+		distance += ai * b[i]
 	}
 
 	return distance
